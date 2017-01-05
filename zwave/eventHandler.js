57cff870a9627a568cfe4a47694046a6bc1f4c60
Hugo Caille <hugo@caille.me>
2017-01-05 12:13:08 +0100

    Scaffold eventHandler

---
+++ zwave/eventHandler.js
@@ -0,0 +1,54 @@
+const handleNodeAdded = (channel, exchange, nodeId) => {
+
+}
+
+const handleNodeRemoved = (channel, exchange, nodeId) => {
+
+}
+
+const handleNodeAvailable = (channel, exchange, nodeId, nodeInfo) => {
+
+}
+
+const handleNodeReady = (channel, exchange, nodeId, nodeInfo) => {
+
+}
+
+const handleNodeEvent = (channel, exchange, nodeId, data) => {
+
+}
+
+const handleValueAdded = (channel, exchange, nodeId, commandClass, valueId) => {
+
+}
+
+const handleValueChanged = (channel, exchange, nodeId, commandClass, valueId) => {
+
+}
+
+const handleValueRefreshed = (channel, exchange, nodeId, commandClass, valueId) => {
+
+}
+
+const handleValueRemoved = (channel, exchange, nodeId, commandClass, instance, index) => {
+
+}
+
+const handleControllerCommand = (nodeId, ctrlState, ctrlError, helpmsg) => {
+
+}
+
+export default function (zwave, channel, exchange) {
+  const wrapper = (cb) => (...args) => cb(channel, exchange, ...args);
+
+  zwave.on('node added', wrapper(handleNodeAdded));
+  zwave.on('node removed', wrapper(handleNodeRemoved));
+  zwave.on('node available', wrapper(handleNodeAvailable));
+  zwave.on('node ready', wrapper(handleNodeReady));
+  zwave.on('node event', wrapper(handleNodeEvent));
+  zwave.on('value added', wrapper(handleValueAdded));
+  zwave.on('value changed', wrapper(handleValueChanged));
+  zwave.on('value refreshed', wrapper(handleValueChanged));
+  zwave.on('value removed', wrapper(handleValueRemoved));
+  zwave.on('controller command', wrapper(handleControllerCommand));
+}
--- zwave/index.js
+++ zwave/index.js
@@ -1,6 +1,6 @@
 import amqp, { Channel } from 'amqplib';
 import OZW from 'openzwave-shared';
-import commandHandler from './commandHandler';
+// import commandHandler from './commandHandler';
 import eventHandler from './eventHandler';
 
 const zwave = new OZW();
@@ -9,13 +9,13 @@ amqp.connect('amqp://zwave:zwave@192.168.2.1/').then(connection => {
   connection.createChannel().then(channel => {
     channel.assertExchange('zwave-events', 'fanout', {durable: true}).then(info => {
       const { exchange } = info;
-      eventHandler(channel, exchange);
+      eventHandler(zwave, channel, exchange);
     });
 
     zwave.on('driver ready', (homeId) => {
       channel.assertQueue(`zwave-${homeId}`, {durable: true}).then(info => {
         const { queue } = info;
-        channel.consume(queue, commandHandler);
+        // channel.consume(queue, commandHandler);
       });
     });
     zwave.connect('/dev/ttyAMA0');
