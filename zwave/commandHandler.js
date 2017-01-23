a1a7b9ff671741440b1cd40196a6def6458d03f8
Hugo Caille <hugo@caille.me>
2017-01-23 02:27:09 +0100

    Add network inclusion/exclusion event handling in ZWave agent

--- zwave/commandHandler.js
+++ zwave/commandHandler.js
@@ -1,5 +1,25 @@
-module.exports = function (data) {
-  const { content, properties } = data;
-  const payload = JSON.parse(data.toString());
-  console.log(payload);
+function addNode(zwave, content, ack) {
+  const doSecurity = content.secure ? content.secure : false;
+  zwave.addNode({ doSecurity });
+  ack();
+}
+
+function removeNode(zwave, content, ack) {
+  zwave.removeNode();
+  ack();
+}
+
+const eventMappings = {
+  ADD_NODE: addNode,
+  REMOVE_NODE: removeNode
+}
+
+module.exports = (channel, zwave) => (message) => {
+  const ack = () => channel.ack(message);
+  const content = JSON.parse(message.content);
+  if (content.event && eventMappings[content.event]) {
+    eventMappings[content.event](zwave, content, ack);
+  } else {
+    ack();
+  }
 }
